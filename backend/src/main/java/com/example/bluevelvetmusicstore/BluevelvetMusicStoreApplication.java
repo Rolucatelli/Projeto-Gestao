fe33267ed4105c78266b3673419a9ff997ad77e6
unipiotto <vinicius.boiago@sou.unifal-mg.edu.br>
2024-11-09 17:28:41 -0300

    Initial commit with project setup
    - Set up basic project structure with src, docs, and test folders
    - Initialized version control with .gitignore for common files

---
+++ backend/src/main/java/com/example/bluevelvetmusicstore/BluevelvetMusicStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.bluevelvetmusicstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluevelvetMusicStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BluevelvetMusicStoreApplication.class, args);
+    }
+
+}
