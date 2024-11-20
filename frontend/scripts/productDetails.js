16ff054e3e7ed79c76c44cc0a8381d7e7c8ac36b
Rolucatelli <rolucatelli@gmail.com>
2024-11-20 18:38:37 -0300

    Finished the integration with the backend

--- frontend/scripts/productDetails.js
+++ frontend/scripts/productDetails.js
@@ -36,40 +36,61 @@ document.addEventListener("DOMContentLoaded", () => {
 	new Carousel(".carousel", ".prev", ".next");
 });
 
-async function getProducts(url) {
-    try {
-        // Fetch data from API
-        const response = await fetch(url);
-        if (!response.ok) { throw new Error(`HTTP error! status: ${response.status}`); }
+async function getProducts() {
+	try {
+		const params = new URLSearchParams(window.location.search);
 
-        const data = await response.json();
-        console.log(data);
+		const url = `http://localhost:3000/api/product/v1/${params.get("id")}`;
+
+		console.log(url);
+		// Fetch data from API
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+
+		const data = await response.json();
+		console.log(data);
 		return data;
-
-    } catch (err) {
-        console.error('Error fetching products:', err);
-    }
+	} catch (err) {
+		console.error("Error fetching products:", err);
+	}
+	return product;
 }
 
-const productName = document.getElementById("product-name");
-const productDescription = document.getElementById("product-description");
-const productCategory = document.getElementById("product-category");
-const productBrand = document.getElementById("product-brand");
-const productListPrice = document.getElementById("product-list-price");
-const productCost = document.getElementById("product-cost");
-const productDiscountPercent = document.getElementById("product-discount-percent");
-const productLength = document.getElementById("product-length");
-const productWidth = document.getElementById("product-width");
-const productHeight = document.getElementById("product-height");
-const productWeight = document.getElementById("product-weight");
-const productActive = document.getElementById("product-active");
-const productStockAmount = document.getElementById("product-stock-amount");
+function renderImages(images) {
+	return images
+		.map((image) => {
+			return `<img src="${image.url}" alt="Product image">`;
+		})
+		.join("");
+}
 
-const url = "http://localhost:3000/products/1";
-
-getProducts(url).then(data => {
+getProducts().then((data) => {
+	const productName = document.getElementById("product-name");
+	const productShortDescription = document.getElementById(
+		"product-short-description"
+	);
+	const productImages = document.getElementsByClassName("carousel-images")[0];
+	const productFullDescription = document.getElementById(
+		"product-full-description"
+	);
+	const productCategory = document.getElementById("product-category");
+	const productBrand = document.getElementById("product-brand");
+	const productListPrice = document.getElementById("product-list-price");
+	const productCost = document.getElementById("product-cost");
+	const productDiscountPercent = document.getElementById(
+		"product-discount-percent"
+	);
+	const productLength = document.getElementById("product-length");
+	const productWidth = document.getElementById("product-width");
+	const productHeight = document.getElementById("product-height");
+	const productWeight = document.getElementById("product-weight");
+	const productActive = document.getElementById("product-active");
+	const productStockAmount = document.getElementById("product-stock-amount");
 	productName.innerHTML = data.name;
-	productDescription.innerHTML = data.description;
+	productShortDescription.innerHTML = data.shortDescription;
+	productFullDescription.innerHTML = data.fullDescription;
 	productCategory.innerHTML = data.category;
 	productBrand.innerHTML = data.brand;
 	productListPrice.innerHTML = data.listPrice;
@@ -79,6 +100,8 @@ getProducts(url).then(data => {
 	productWidth.innerHTML = data.width;
 	productHeight.innerHTML = data.height;
 	productWeight.innerHTML = data.weight;
-	productActive.innerHTML = data.active;
+	productActive.checked = data.active;
 	productStockAmount.innerHTML = data.stockAmount;
+
+	productImages.innerHTML = renderImages(data.images);
 });
