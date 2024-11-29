c755c080f71422b60de07d807287a7c709e12f7e
JoaoBLeite <joao222barbosa@gmail.com>
2024-11-29 10:26:02 -0300

    Updating exception response

--- backend/src/main/java/com/example/bluevelvetmusicstore/controller/UserController.java
+++ backend/src/main/java/com/example/bluevelvetmusicstore/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.bluevelvetmusicstore.controller;
 
 import com.example.bluevelvetmusicstore.model.entities.User;
 import com.example.bluevelvetmusicstore.model.vo.CreateUserVO;
+import com.example.bluevelvetmusicstore.model.vo.ExceptionVO;
 import com.example.bluevelvetmusicstore.service.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +23,7 @@ public class UserController {
       User createdUser = userService.createUser(createUserVO);
       return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     } catch (IllegalArgumentException ex) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionVO(ex.getMessage()));
     }
   }
 }
