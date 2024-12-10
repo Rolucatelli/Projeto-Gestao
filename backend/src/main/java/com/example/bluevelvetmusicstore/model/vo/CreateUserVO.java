1ae7554e8da191a1248816b6903665ce83e803f0
LBonardi23 <lbonardi2312@gmail.com>
2024-12-10 19:15:32 -0300

    Refactor user image upload implementation

--- backend/src/main/java/com/example/bluevelvetmusicstore/model/vo/CreateUserVO.java
+++ backend/src/main/java/com/example/bluevelvetmusicstore/model/vo/CreateUserVO.java
@@ -3,10 +3,12 @@ package com.example.bluevelvetmusicstore.model.vo;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import java.util.List;
 
 public record CreateUserVO(
     @Email String email,
     @NotBlank @Size(min = 2, max = 60) String firstName,
     @NotBlank @Size(min = 2, max = 60) String lastName,
     @NotBlank String password,
-    String photo) {}
+    List<RoleVO> roles,
+    PhotoVO photo) {}
