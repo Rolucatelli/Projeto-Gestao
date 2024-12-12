e6405ba4fa1456484a922680f18f6c6ed7052195
Rolucatelli <rolucatelli@gmail.com>
2024-12-12 03:59:47 -0300

    Updated the productDetails page to contain product details

--- frontend/scripts/productDetails.js
+++ frontend/scripts/productDetails.js
@@ -65,6 +65,22 @@ function renderImages(images) {
 		.join("");
 }
 
+function renderDetails(details) {
+	return details
+		.map((detail) => {
+			return `
+			<div class="detail">
+				<h3>Detail ${details.indexOf(detail) + 1}:</h3>
+				<div class="row">
+					<span id="detailName">${detail.name}:</span>
+					<span id="detailValue">${detail.value}</span>
+				</div>
+			</div>
+		`;
+		})
+		.join("");
+}
+
 getProducts().then((data) => {
 	const productName = document.getElementById("product-name");
 	const productShortDescription = document.getElementById(
@@ -87,6 +103,7 @@ getProducts().then((data) => {
 	const productWeight = document.getElementById("product-weight");
 	const productActive = document.getElementById("product-active");
 	const productStockAmount = document.getElementById("product-stock-amount");
+	const detailsList = document.getElementsByClassName(".details-section")[0];
 	productName.innerHTML = data.name;
 	productShortDescription.innerHTML = data.shortDescription;
 	productFullDescription.innerHTML = data.fullDescription;
@@ -101,6 +118,7 @@ getProducts().then((data) => {
 	productWeight.innerHTML = data.dimensions.weight;
 	productActive.checked = data.isActive;
 	productStockAmount.innerHTML = data.stockAmount;
+	detailsList.innerHTML = renderDetails(data.details);
 
 	productImages.innerHTML = renderImages(data.images);
 });
